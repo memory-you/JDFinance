4e0efa443d06e53ced367c93ad509afefaed99e0
memory-you <869529706@qq.com>
2019-04-20 13:21:22 +0800

    第二次

---
+++ app/js/core/slider.vue
@@ -0,0 +1,52 @@
+<template>
+    <section :class="cname">
+        <swiper :options="swiperOption" :not-next-tick="swiperOption.notNextTick">
+            <swiper-slide v-for="item in items" :key="item.href">
+                <router-link :to="{name:item.href}">
+                    <img :src="item.src">
+                </router-link>
+            </swiper-slide>
+            <div class="swiper-pagination" slot="pagination" v-if="swiperOption.pagination"></div>
+        </swiper>
+    </section>
+</template>
+
+<script>
+import { swiper, swiperSlide } from "vue-awesome-swiper";
+export default {
+    components: {
+        swiper,
+        swiperSlide
+    },
+    props: {
+        cname: {
+            type: String,
+            default: ""
+        },
+        swiperOption: {
+            type: Object,
+            default() {
+                return {
+                    autoplay: true,
+                    loop: true,
+                    pagination: {
+                        el: ".swiper-pagination"
+                    },
+                    notNextTick: false
+                };
+            }
+        },
+        items: {
+            type: Array,
+            default() {
+                // [{href:"",src:""}]
+                return [];
+            }
+        }
+    }
+};
+</script>
+
+<style  lang="scss">
+@import "~swiper/dist/css/swiper.css";
+</style>
---
+++ app/js/public/header.vue
@@ -0,0 +1,53 @@
+<template>
+    <!-- 在style标签标明 module后  可以使用$style去绑定类名  -->
+    <div :class="$style.header">
+        <span :class="$style.left">
+            <em>注册</em>&nbsp;|&nbsp;
+            <em>登录</em>
+        </span>
+        <btn :class="$style.btnDownload">APP下载</btn>
+    </div>
+</template>
+
+<script>
+import btn from "../core/btn.vue";
+export default {
+    components: {
+        btn
+    }
+};
+</script>
+
+<style  lang="scss" module>
+.header {
+    height: 100px;
+    line-height: 100px;
+    color: #666;
+    font-size: 32px;
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    background: #fff url("//m.jr.jd.com/spe/qyy/main/images/jr-logo.png")
+        no-repeat center center;
+    background-size: auto 50%;
+    z-index: 100;
+    .left {
+        font-size: 28px;
+        height: 30px;
+        line-height: 30px;
+        margin: 17px 0 0 18px;
+    }
+    .btnDownload {
+        font-size: 24px;
+        height: 56px;
+        line-height: 56px;
+        min-width: 120px;
+        border-radius: 4px;
+        padding: 0;
+        border-width: 0;
+        margin: 28px 24px 0 0;
+        float: right;
+    }
+}
+</style>
